f3665a58bcbc94017f86ce7a4a99b521889aa225
victorG <peter200970@hotmail.com>
2020-07-31 15:49:38 -0500

    select_from_nobel quizz completed and saved

--- select_from_nobel.sql
+++ select_from_nobel.sql
@@ -39,4 +39,53 @@ SELECT winner, subject
 FROM nobel
 WHERE yr=1984
 ORDER BY subject IN ('Physics','Chemistry'), subject,winner
+
+/*QUIZZ*/
                   
+/*1*/             
+SELECT winner FROM nobel
+WHERE winner LIKE 'C%' AND winner LIKE '%n'
+
+/*2*/
+SELECT COUNT(subject) FROM nobel
+WHERE subject = 'Chemistry'
+AND yr BETWEEN 1950 and 1960
+
+/*3*/ 
+SELECT COUNT(DISTINCT yr) FROM nobel
+WHERE yr NOT IN (SELECT DISTINCT yr FROM nobel WHERE subject = 'Medicine')
+
+/*4*/ 
+SELECT subject, winner FROM nobel WHERE winner LIKE 'Sir%' AND yr LIKE '196%'
+
+/* 
+Medicine	Sir John Eccles
+Medicine	Sir Frank Macfarlane Burnet */
+
+/*5*/
+SELECT yr FROM nobel
+ WHERE yr NOT IN(SELECT yr 
+                   FROM nobel
+                 WHERE subject IN ('Chemistry','Physics'))
+
+/*6*/
+SELECT DISTINCT yr
+  FROM nobel
+ WHERE subject='Medicine' 
+   AND yr NOT IN(SELECT yr FROM nobel 
+                  WHERE subject='Literature')
+   AND yr NOT IN (SELECT yr FROM nobel
+                   WHERE subject='Peace')
+
+/*7*/ 
+SELECT subject, COUNT(subject) 
+FROM nobel 
+WHERE yr ='1960' 
+GROUP BY subject
+
+/*
+Chemistry	1
+Literature	1
+Medicine	2
+Peace	1
+Physics	1*/
