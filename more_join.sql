51f6ec738211ae0cdc32f2c8626f54dfdfd9717f
victorG <peter200970@hotmail.com>
2020-08-03 14:42:15 -0500

    more_join exercises and quizz completed

--- more_join.sql
+++ more_join.sql
@@ -62,3 +62,50 @@ WHERE movie.id IN (SELECT movie.id FROM movie
                    JOIN actor ON casting.actorid = actor.id 
                    WHERE name LIKE 'Art Garfunkel')
 AND name != 'Art Garfunkel';
+/*QUIZZ*/
+
+/*1*/
+SELECT name
+FROM actor INNER JOIN movie ON actor.id = director
+WHERE gross < budget
+
+/*2*/
+SELECT *
+FROM actor JOIN casting ON actor.id = actorid
+JOIN movie ON movie.id = movieid
+
+/*3*/
+SELECT name, COUNT(movieid)
+FROM casting JOIN actor ON actorid=actor.id
+WHERE name LIKE 'John %'
+GROUP BY name ORDER BY 2 DESC
+
+/*4*/
+SELECT title 
+FROM movie JOIN casting ON (movieid=movie.id)
+JOIN actor   ON (actorid=actor.id)
+WHERE name='Paul Hogan' AND ord = 1
+/*Table-B
+"Crocodile" Dundee
+Crocodile Dundee in Los Angeles
+Flipper
+Lightning Jack*/
+
+/*5*/
+SELECT name
+FROM movie JOIN casting ON movie.id = movieid
+JOIN actor ON actor.id = actorid
+WHERE ord = 1 AND director = 351
+
+/*6*/
+/*link the director column in movies with the primary key in actor
+connect the primary keys of movie and actor via the casting table*/
+
+/*7*/
+SELECT title, yr 
+FROM movie, casting, actor 
+WHERE name='Robert De Niro' AND movieid=movie.id AND actorid=actor.id AND ord = 3
+/*Table-B
+A Bronx Tale	1993
+Bang the Drum Slowly	1973
+Limitless	2011*/
